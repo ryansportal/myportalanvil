9effc18f0cde4a5817367f1c6db1d19cfd906fe7
ryan.burleigh@payprop.co.uk <ryan.burleigh@payprop.co.uk>
2024-08-07 10:40:18 +0000

    Created Form1.Home and ServerModule1; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
@@ -9,5 +10,40 @@ class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+    
+    
     # Any code you write here will run before the form opens.
+    self.category_dd.items = [(r['name'], r) for r in app_tables.categories.search()]
+  
+    
+
+  def category_dd_change(self, **event_args):
+    """This method is called when an item is selected"""
+
+  def validate_fields(self):
+    suggestion = self.suggestion_box.text
+    drop_down = self.category_dd.selected_value
+    if not suggestion or not drop_down:
+      alert('Please complete all fields', title='Validation Error')
+      return False
+    return True
+  
+  def clear_inputs (self):
+    self.suggestion_box.text = ""
+    self.category_dd.selected_value = None
+
+    
+  def submit_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    if self.validate_fields():
+      anvil.server.call( 'add_suggestion', self.suggestion_box.text, self.category_dd.selected_value)
+      alert("Form submitted")
+      self.clear_inputs()
+
+  
+
+
+  
+
+
+  
---
+++ server_code/ServerModule1.py
@@ -0,0 +1,8 @@
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+@anvil.server.callable
+def add_suggestion(text, category):
+  app_tables.suggestions_2.add_row(text=text, category=category)
