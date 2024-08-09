eec426ca089eda6baebaff366c0041b6cf4aee3b
ryan.burleigh@payprop.co.uk <ryan.burleigh@payprop.co.uk>
2024-08-09 12:31:18 +0000

    Edited PayProp_Requests

--- client_code/PayProp_Requests/__init__.py
+++ client_code/PayProp_Requests/__init__.py
@@ -11,19 +11,33 @@ class PayProp_Requests(PayProp_RequestsTemplate):
         # Set Form properties and Data Bindings
         self.init_components(**properties)
 
+          # Example coordinates for the marker
+        latitude = 51.271240
+        longitude = 0.191750
 
+        # Create a marker using the correct syntax
+        marker = GoogleMap.Marker(
+            position=GoogleMap.LatLng(latitude, longitude),  # Position of the marker
+            title="UK",  # Title when hovering over the marker
+            label="UK"  # A short label for the marker
+        )
 
+        latitudesa = -30.559483
+        longitudesa = 22.937506
+        markersa = GoogleMap.Marker(
+        position=GoogleMap.LatLng(latitudesa, longitudesa),
+        title="SA",
+        label="SA",
+        icon= "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAP1BMVEXbNSn////ZHQb22djYEgDaKBj109LroJ3niobaKxzZIAzzzcvaJRTWAADwubfbLiHjc2733tzlg37gX1jrpKEDjV34AAABNElEQVR4nO3bsW6DMBiF0RQIgZKE0Ob9n7WLWWoGTEX+UJ0zX1n6Vss+nQAAAAAAAAAAAAAAAADgvVyrvV2DA+/13u6xiVX9sbe6UqhQoUKFChUqVKhQoUKFChXGFA7tVsNBCm+fW90OUthsPq5R+HoKCykMoLCQwgAKCykMoLCQwgAKCykMoLCQwgAKCykMoLCQwgAKCykMoLCQwgAKCykMoLDQgQvHhYcXYz47buH4aIff2keeeNzCvluYdX22UxhAYaJwpjCAwkThTGEAhYnCmcIAChOFM4UB/lL41fyjPzOLhdNx/z2tLFxJ4c4UJgoVBlKYKFQYSGGiUGEghYnCdy6cdi+cYgv77pJ5nrPZ+P3MdystXFi9VH/OLczGhdlKwYEAAAAAAAAAAAAAAAAAkPkBqLktxBvvZ98AAAAASUVORK5CYII="
+      )
 
+      
+        # Add the marker to the GoogleMap component
+        self.map_1.add_component(marker)
+        self.map_1.add_component(markersa)
 
-
-
-
-
-
-
-
-
-  
+        
+ 
     def home_button_click(self, **event_args):
      open_form('Home')
     def feedback_button_click(self, **event_args):
@@ -42,4 +56,5 @@ class PayProp_Requests(PayProp_RequestsTemplate):
       open_form('South_Africa')
     def link_4_click(self, **event_args):
       open_form('US_CA')
-  
+
+     
