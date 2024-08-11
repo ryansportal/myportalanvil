ce3be4ed34535a918a1b9b4e082d7bd3a597234a
ryan.burleigh@payprop.co.uk <ryan.burleigh@payprop.co.uk>
2024-08-11 14:02:07 +0000

    Edited landing_page

--- client_code/landing_page/__init__.py
+++ client_code/landing_page/__init__.py
@@ -16,22 +16,29 @@ class landing_page(landing_pageTemplate):
     self.init_components(**properties)
 
    
-  #GET CURRENT USER
-    user = anvil.users.get_user()
-    self.user_state(user)
-  
-# Log In 
+    def check_user_state(self):
+        # GET CURRENT USER
+        user = anvil.users.get_user()
+        if user is not None:
+            # Redirect to 'Home' if the user is already logged in
+            if not self.form_redirected:
+                open_form('Home')
+                self.form_redirected = True
+        else:
+            # Show login button if no user is logged in
+            self.button_login.visible = True
     
-  def button_login_click(self, **event_args):
-    user = anvil.users.login_with_form(allow_cancel=True)
-    self.user_state(user)
+    # Log In
+    def button_login_click(self, **event_args):
+        user = anvil.users.login_with_form(allow_cancel=True)
+        self.check_user_state()  # Check user state after login
     
+    # Log Out (Assuming logout is handled elsewhere and this form is not used for logout)
+    def button_logout_click(self, **event_args):
+        anvil.users.logout()
+        open_form('landing_page')  # Redirect to landing_page after logging out
 
-  def user_state(self, user):
-    self.button_login.visible = True
-  
-
-  # Redirect to another form if the user is logged in
-    if user is not None:
-      open_form('Home')  # Replace 'new_form_name' with the actual form you want to open
-      
+    # Ensure proper user state handling on form load
+    def user_state(self, user):
+        self.button_login.visible = user is None
+        self.button_logout.visible = user is not None
