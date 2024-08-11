7086f28ef0b63d3b4ecd5b064c4a36b9e5b9adf3
ryan.burleigh@payprop.co.uk <ryan.burleigh@payprop.co.uk>
2024-08-11 14:29:19 +0000

    Edited landing_page

--- client_code/landing_page/__init__.py
+++ client_code/landing_page/__init__.py
@@ -17,21 +17,26 @@ class landing_page(landing_pageTemplate):
 
    
   #GET CURRENT USER
-    user = anvil.users.get_user()
-    self.user_state(user)
+   # user = anvil.users.get_user()
+   # self.user_state(user)
   
 # Log In 
     
-  def button_login_click(self, **event_args):
-    user = anvil.users.login_with_form(allow_cancel=True)
-    self.user_state(user)
+ # def button_login_click(self, **event_args):
+  #  user = anvil.users.login_with_form(allow_cancel=True)
+   # self.user_state(user)
     
 
-  def user_state(self, user):
-    self.button_login.visible = True
-  
-
+  #def user_state(self, user):
+  #  self.button_login.visible = True
+  def button_login_click(self, **event_args):
+    anvil.users.login_with_form()
+    
+    user = anvil.users.get_user()
   # Redirect to another form if the user is logged in
     if user is not None:
       open_form('Home')  # Replace 'new_form_name' with the actual form you want to open
+
+ 
+    
   
