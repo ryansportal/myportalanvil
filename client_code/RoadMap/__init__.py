268b763d72852c4822448cb7128ac7a69f974722
ryan.burleigh@payprop.co.uk <ryan.burleigh@payprop.co.uk>
2024-08-12 08:04:19 +0000

    Edited Feedback, Global, Global.RowTemplate3, Home and 15 more

--- client_code/RoadMap/__init__.py
+++ client_code/RoadMap/__init__.py
@@ -1,8 +1,5 @@
 from ._anvil_designer import RoadMapTemplate
 from anvil import *
-import anvil.facebook.auth
-import anvil.google.auth, anvil.google.drive
-from anvil.google.drive import app_files
 import anvil.users
 import anvil.server
 import anvil.tables as tables
